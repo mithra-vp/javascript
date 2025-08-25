36175acd03b1e97b4c649fb3010f6ee0fe07cb16
mithra-vp <mithravp004@gmail.com>
2025-08-25 13:09:00 +0530

    first commit

---
+++ var.js
@@ -0,0 +1,15 @@
+// var 
+// variables are boxes that store datas and change
+// redclare,reassign ,function scoped
+
+// let
+// block scoped,reassignment  eeth blockil aano code athin appurathik call cheyan pattula
+
+// const
+// block scoped
+
+// "use strict" prevent errors
+
+var x = "mithra";
+var x = "muff";
+console.log(x);
